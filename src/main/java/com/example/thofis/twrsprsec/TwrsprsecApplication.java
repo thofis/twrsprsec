7ea1d255ea8bc958dcbae11d67a68d67e1660873
Thomas Fischer <tf@media-nox.de>
2021-03-07 16:31:05 +0100

    initial commit; /hello unprotected

---
+++ src/main/java/com/example/thofis/twrsprsec/TwrsprsecApplication.java
@@ -0,0 +1,13 @@
+package com.example.thofis.twrsprsec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwrsprsecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwrsprsecApplication.class, args);
+	}
+
+}

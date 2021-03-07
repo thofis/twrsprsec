3dca3b7a52417cbf3153611fe077b25f0296a997
Thomas Fischer <tf@media-nox.de>
2021-03-07 17:15:34 +0100

    basic auth; inmemory user; trace logging

--- src/main/java/com/example/thofis/twrsprsec/WebSecurityConfig.java
+++ src/main/java/com/example/thofis/twrsprsec/WebSecurityConfig.java
@@ -1,9 +1,17 @@
 package com.example.thofis.twrsprsec;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 
 @EnableWebSecurity
 @Configuration
@@ -13,7 +21,31 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		http
 				.authorizeRequests()
 				.anyRequest()
-				.permitAll();
+				.authenticated()
+				.and()
+				.httpBasic();
+	}
+
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.debug(true);
+	}
+
+	@Bean
+	public UserDetailsManager userDetailsManager() {
+		InMemoryUserDetailsManager inMemoryUserDetailsManager = new InMemoryUserDetailsManager();
+		inMemoryUserDetailsManager.createUser(User
+				.builder()
+				.username("Thomas")
+				.password("Passw0rt")
+				.authorities("all")
+				.build());
+		return inMemoryUserDetailsManager;
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return NoOpPasswordEncoder.getInstance();
 	}
 
 }

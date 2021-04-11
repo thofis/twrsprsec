d47f2634514b70ef83a8392394a93c06cff03cc4
Thomas Fischer <tf@media-nox.de>
2021-04-11 17:49:46 +0200

    switched to storing jwt in cookie and therefore added csrf-protection

--- src/main/java/com/example/thofis/twrsprsec/WebSecurityConfig.java
+++ src/main/java/com/example/thofis/twrsprsec/WebSecurityConfig.java
@@ -1,8 +1,13 @@
 package com.example.thofis.twrsprsec;
 
+import java.util.List;
+
+import javax.servlet.http.Cookie;
+
 import com.example.thofis.twrsprsec.security.JwtAuthenticationFilter;
 import com.example.thofis.twrsprsec.security.JwtAuthorizationFilter;
 import com.example.thofis.twrsprsec.security.JwtConfiguration;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,91 +19,120 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.cors.CorsConfiguration;
 
-import java.util.List;
-
-import static com.example.thofis.twrsprsec.security.Permission.*;
+import static com.example.thofis.twrsprsec.security.JwtAuthenticationFilter.JWT_COOKIE_NAME;
+import static com.example.thofis.twrsprsec.security.Permission.CREATE_ARTICLE;
+import static com.example.thofis.twrsprsec.security.Permission.CREATE_ORDER;
+import static com.example.thofis.twrsprsec.security.Permission.DELETE_ARTICLE;
+import static com.example.thofis.twrsprsec.security.Permission.DELETE_ORDER;
+import static com.example.thofis.twrsprsec.security.Permission.READ_ARTICLE;
+import static com.example.thofis.twrsprsec.security.Permission.READ_ORDER;
+import static com.example.thofis.twrsprsec.security.Permission.WRITE_ARTICLE;
+import static com.example.thofis.twrsprsec.security.Permission.WRITE_ORDER;
 import static com.example.thofis.twrsprsec.security.Role.ADMIN;
 import static com.example.thofis.twrsprsec.security.Role.USER;
-import static org.springframework.http.HttpMethod.*;
+import static org.springframework.http.HttpMethod.DELETE;
+import static org.springframework.http.HttpMethod.GET;
+import static org.springframework.http.HttpMethod.POST;
+import static org.springframework.http.HttpMethod.PUT;
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 @EnableWebSecurity
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-  @Autowired
-  private JwtConfiguration jwtConfiguration;
+	@Autowired
+	private JwtConfiguration jwtConfiguration;
 
-  @Override
-  protected void configure(HttpSecurity http) throws Exception {
-    // @formatter:off
-    http
-        .addFilter(new JwtAuthenticationFilter(authenticationManager(),
-                                               jwtConfiguration.getAudience(),
-                                               jwtConfiguration.getIssuer(),
-                                               jwtConfiguration.getSecret(),
-                                               jwtConfiguration.getType()))
-        .addFilter(new JwtAuthorizationFilter(authenticationManager(),
-                                              jwtConfiguration.getAudience(),
-                                              jwtConfiguration.getIssuer(),
-                                              jwtConfiguration.getSecret(),
-                                              jwtConfiguration.getType()))
-        .authorizeRequests()
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		// @formatter:off
+		http
+				.addFilter(new JwtAuthenticationFilter(authenticationManager(),
+						jwtConfiguration.getAudience(),
+						jwtConfiguration.getIssuer(),
+						jwtConfiguration.getSecret(),
+						jwtConfiguration.getType()))
+				.addFilter(new JwtAuthorizationFilter(authenticationManager(),
+						jwtConfiguration.getAudience(),
+						jwtConfiguration.getIssuer(),
+						jwtConfiguration.getSecret(),
+						jwtConfiguration.getType()))
+				.authorizeRequests()
 
-        .mvcMatchers(GET, "/articles/**").hasAuthority(READ_ARTICLE.name())
-        .mvcMatchers(POST, "/articles/**").hasAuthority(CREATE_ARTICLE.name())
-        .mvcMatchers(PUT, "/articles/**").hasAuthority(WRITE_ARTICLE.name())
-        .mvcMatchers(DELETE, "/articles/**").hasAuthority(DELETE_ARTICLE.name())
+				.mvcMatchers(GET, "/articles/**").hasAuthority(READ_ARTICLE.name())
+				.mvcMatchers(POST, "/articles/**").hasAuthority(CREATE_ARTICLE.name())
+				.mvcMatchers(PUT, "/articles/**").hasAuthority(WRITE_ARTICLE.name())
+				.mvcMatchers(DELETE, "/articles/**").hasAuthority(DELETE_ARTICLE.name())
 
-        .mvcMatchers(GET, "/orders/**").hasAuthority(READ_ORDER.name())
-        .mvcMatchers(POST, "/orders/**").hasAuthority(CREATE_ORDER.name())
-        .mvcMatchers(PUT, "/orders/**").hasAuthority(WRITE_ORDER.name())
-        .mvcMatchers(DELETE, "/orders/**").hasAuthority(DELETE_ORDER.name())
+				.mvcMatchers(GET, "/orders/**").hasAuthority(READ_ORDER.name())
+				.mvcMatchers(POST, "/orders/**").hasAuthority(CREATE_ORDER.name())
+				.mvcMatchers(PUT, "/orders/**").hasAuthority(WRITE_ORDER.name())
+				.mvcMatchers(DELETE, "/orders/**").hasAuthority(DELETE_ORDER.name())
 
-		.mvcMatchers(GET, "/hello-admin").hasRole(ADMIN.name())
-		.mvcMatchers(GET, "/hello-user").hasRole(USER.name())
+				.mvcMatchers(GET, "/hello-admin").hasRole(ADMIN.name())
+				.mvcMatchers(GET, "/hello-user").hasRole(USER.name())
 
-        .mvcMatchers("/hello").authenticated()
+				.mvcMatchers("/hello").authenticated()
 //        .mvcMatchers(POST,"/api/login").permitAll()
 
-        .mvcMatchers("/", "/h2-console/**", "/frontend/**").permitAll()
+				.mvcMatchers("/", "/h2-console/**", "/frontend/**", "jwt-from-cookie", "logout").permitAll()
 
-        .anyRequest().denyAll();
+				.anyRequest().denyAll();
 //        .httpBasic();
 
-        http.exceptionHandling().authenticationEntryPoint(unauthorizedEntryPoint());
+		http
+				.logout(logout -> logout
+						.logoutUrl("/logout")
+						.addLogoutHandler((request, response, auth) -> {
+									for (Cookie cookie : request.getCookies()) {
+										if (cookie.getName().equals(JWT_COOKIE_NAME)) {
+											Cookie cookieToDelete = new Cookie(JWT_COOKIE_NAME, null);
+											cookieToDelete.setMaxAge(0);
+											response.addCookie(cookieToDelete);
+										}
+									}
+								}
+						));
 
-        http
-        .sessionManagement()
-        .sessionCreationPolicy(STATELESS);
+		http.exceptionHandling().authenticationEntryPoint(unauthorizedEntryPoint());
 
-        http.cors().configurationSource(request -> {
+		http
+				.sessionManagement()
+				.sessionCreationPolicy(STATELESS);
+
+		http.cors().configurationSource(request -> {
 			var cors = new CorsConfiguration();
 			cors.setAllowedOrigins(List.of("http://localhost:9000"));
-			cors.setAllowedMethods(List.of("GET","POST", "PUT", "DELETE", "OPTIONS"));
+			cors.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 			cors.setAllowedHeaders(List.of("*"));
 			return cors;
 		});
-        http.csrf().disable();
-        http.headers().frameOptions().disable();
-    // @formatter:on
-  }
 
-  @Override
-  public void configure(WebSecurity web) throws Exception {
-    web.debug(true);
-  }
+//		http.csrf().disable();
+		http.csrf()
+				.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+
+		http.headers().frameOptions().disable();
+
+		// @formatter:on
+	}
+
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.debug(true);
+	}
 
 
-  @Bean
-  public PasswordEncoder passwordEncoder() {
-    return PasswordEncoderFactories.createDelegatingPasswordEncoder();
-  }
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+	}
 
-  @Bean
-  public HttpStatusEntryPoint unauthorizedEntryPoint() {
-    return new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED);
-  }
+	@Bean
+	public HttpStatusEntryPoint unauthorizedEntryPoint() {
+		return new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED);
+	}
 }
--- src/main/java/com/example/thofis/twrsprsec/security/JwtAuthorizationFilter.java
+++ src/main/java/com/example/thofis/twrsprsec/security/JwtAuthorizationFilter.java
@@ -1,11 +1,21 @@
 package com.example.thofis.twrsprsec.security;
 
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpHeaders;
+
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -13,80 +23,71 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.util.ObjectUtils;
 
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
-
+import static com.example.thofis.twrsprsec.security.JwtAuthenticationFilter.JWT_COOKIE_NAME;
 import static java.util.stream.Collectors.toSet;
 
 
 @Slf4j
 public class JwtAuthorizationFilter extends BasicAuthenticationFilter {
 
-  private final String jwtSecret;
-  private final String jwtIssuer;
-  private final String jwtType;
-  private final String jwtAudience;
+	private final String jwtSecret;
 
 
-  public JwtAuthorizationFilter(AuthenticationManager authenticationManager,
-                                String jwtAudience, String jwtIssuer, String jwtSecret, String jwtType) {
-    super(authenticationManager);
-    this.jwtAudience = jwtAudience;
-    this.jwtIssuer = jwtIssuer;
-    this.jwtSecret = jwtSecret;
-    this.jwtType = jwtType;
-  }
+	public JwtAuthorizationFilter(AuthenticationManager authenticationManager,
+			String jwtAudience, String jwtIssuer, String jwtSecret, String jwtType) {
+		super(authenticationManager);
+		this.jwtSecret = jwtSecret;
+	}
 
-  @Override
-  protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
-    UsernamePasswordAuthenticationToken authenticationToken = parseToken(request);
-    if (authenticationToken != null) {
-      SecurityContextHolder.getContext()
-                           .setAuthentication(authenticationToken);
-    }
-    chain.doFilter(request, response);
-  }
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
+		UsernamePasswordAuthenticationToken authenticationToken = parseToken(request);
+		if (authenticationToken != null) {
+			SecurityContextHolder.getContext()
+					.setAuthentication(authenticationToken);
+		}
+		chain.doFilter(request, response);
+	}
 
-  private UsernamePasswordAuthenticationToken parseToken(HttpServletRequest request) {
-    String token = request.getHeader(HttpHeaders.AUTHORIZATION);
-    if (token != null && token.startsWith("Bearer ")) {
-      log.info("processing bearer token: {}", token);
-      String claims = token.replace("Bearer ", "");
-      try {
-        Jws<Claims> claimsJws = Jwts.parser()
-                                    .setSigningKey(jwtSecret.getBytes())
-                                    .parseClaimsJws(claims);
-        String username = (String) claimsJws.getBody()
-                                            .get("subject");
-        if ("".equals(username) || username == null) {
-          return null;
-        }
-        Collection<SimpleGrantedAuthority> authorities = authoritiesFromString((String) claimsJws.getBody()
-                                                                                                 .get("authorities"));
-        log.info("authorizing user: {} with authorities: {}", username, authorities);
-        // authorities could alternatively retrieved from db (findByUsername)
-        return new UsernamePasswordAuthenticationToken(username, null, authorities);
-      } catch (JwtException e) {
-        log.warn("Some exception: {} failed : {}", token, e.getMessage());
-      }
-    }
-    return null;
-  }
+	private UsernamePasswordAuthenticationToken parseToken(HttpServletRequest request) {
+	// @formatter:off
+	return Arrays.
+			stream(request.getCookies())
+			.filter(cookie -> JWT_COOKIE_NAME.equals(cookie.getName()))
+			.findAny()
+			.map(cookie -> {
+						String claims = cookie.getValue();
+						try {
+							Jws<Claims> claimsJws = Jwts.parser()
+									.setSigningKey(jwtSecret.getBytes())
+									.parseClaimsJws(claims);
+							String username = (String) claimsJws.getBody()
+									.get("subject");
+							if ("".equals(username) || username == null) {
+								return null;
+							}
+							Collection<SimpleGrantedAuthority> authorities = authoritiesFromString((String) claimsJws.getBody()
+									.get("authorities"));
+							log.info("authorizing user: {} with authorities: {}", username, authorities);
+							// authorities could alternatively retrieved from db (findByUsername)
+							return new UsernamePasswordAuthenticationToken(username, null, authorities);
+						}
+						catch (JwtException e) {
+							log.warn("Some exception: {} failed : {}", claims, e.getMessage());
+							return null;
+						}
+					}
+			).orElse(null);
+		// @formatter:on
+	}
 
 
-  private Collection<SimpleGrantedAuthority> authoritiesFromString(String authorititiesString) {
-    if (ObjectUtils.isEmpty(authorititiesString)) {
-      return Collections.emptySet();
-    }
-
-    return Arrays.stream(authorititiesString.split("\\|"))
-                 .map(SimpleGrantedAuthority::new)
-                 .collect(toSet());
-  }
+	private Collection<SimpleGrantedAuthority> authoritiesFromString(String authoritiesString) {
+		if (ObjectUtils.isEmpty(authoritiesString)) {
+			return Collections.emptySet();
+		}
+		return Arrays.stream(authoritiesString.split("\\|"))
+				.map(SimpleGrantedAuthority::new)
+				.collect(toSet());
+	}
 }
